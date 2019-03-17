78f6155a6bf2680ca3afa11c2350bc54a50af8bd
Administrator <basma.elsheikh@gmail.com>
2019-03-16 20:20:21 -0400

    test

---
+++ base/logger.py
@@ -0,0 +1,7 @@
+import logging
+
+logging.basicConfig(filename='example.log', format='%(levelname)s: %(asctime)s -- %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', filemode='w',level=logging.DEBUG)
+logging.debug('this message should go to file')
+logging.info('and this')
+logging.warning('and this')
+# logging.log(level=logging.DEBUG)
